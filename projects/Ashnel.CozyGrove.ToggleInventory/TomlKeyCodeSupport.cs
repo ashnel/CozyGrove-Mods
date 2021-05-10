7ae1c54d8d1f3d23f6f9cfc4661e8f96b3e68f37
ashnel <ashleyjnelms@gmail.com>
2021-05-10 09:17:56 -0700

    first commit

---
+++ projects/Ashnel.CozyGrove.ToggleInventory/TomlKeyCodeSupport.cs
@@ -0,0 +1,34 @@
+using System;
+using MelonLoader;
+using MelonLoader.Tomlyn.Model;
+using UnityEngine;
+
+namespace Ashnel.CozyGrove.ToggleInventory
+{
+    internal static class TomlKeyCodeSupport
+    {
+        public static void Install()
+        {
+            try
+            {
+                MelonPreferences.Mapper.RegisterMapper(FromToml, ToToml);
+            }
+            catch
+            {
+            }
+        }
+
+        private static KeyCode FromToml(TomlObject value)
+        {
+            var s = (value as TomlString)?.Value;
+            return string.IsNullOrEmpty(s) == false && Enum.TryParse<KeyCode>(s, out var keycode) != false
+                ? keycode
+                : KeyCode.None;
+        }
+
+        private static TomlObject ToToml(KeyCode value)
+        {
+            return MelonPreferences.Mapper.ToToml(value.ToString());
+        }
+    }
+}
