addca59aaf5c4cdb53b1813aadf9582822468cc5
ashnel <ashleyjnelms@gmail.com>
2021-06-07 15:10:39 -0700

    mod for instant cooking to inventory

---
+++ projects/Ashnel.CozyGrove.InstantCooking/MyMod.cs
@@ -0,0 +1,50 @@
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Ashnel.CozyGrove.InstantCooking
+{
+    public class MyMod : MelonMod
+    {
+        private const string PreferenceCategoryId = "Ashnel.CozyGrove.InstantCooking";
+        private const string PreferenceHotkeyId = "Hotkey";
+
+        private KeyCode _HotKey = KeyCode.Tab;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+
+            TomlKeyCodeSupport.Install();
+            MelonPreferences.CreateCategory(PreferenceCategoryId, $"Toggle HUD Settings");
+            MelonPreferences.CreateEntry(PreferenceCategoryId, PreferenceHotkeyId, _HotKey, "Hotkey");
+            this.ApplyPreferences();
+        }
+
+        private void ApplyPreferences()
+        {
+            _HotKey = MelonPreferences.GetEntryValue<KeyCode>(PreferenceCategoryId, PreferenceHotkeyId);
+        }
+
+        public override void OnPreferencesSaved() => this.ApplyPreferences();
+
+        public override void OnUpdate()
+        {
+            if (SceneManager.GetActiveScene().name == "Game" && GameUI.Instance != null)
+            {
+                Cheats.instantDelivery = true;
+            }
+            // if (this._HotKey != KeyCode.None &&
+            //     SceneManager.GetActiveScene().name == "Game" &&
+            //     Input.GetKeyDown(this._HotKey) == true)
+            // {
+            //     // toggle inventory visibility.
+            //     var instance = GameUI.Instance.inventory;
+            //     if (instance != null)
+            //     {
+            //         Cheats.instantDelivery = true;
+            //     }
+            // }
+        }
+    }
+}
